7036571311c471312edfa21478190cd43142416d
Vivienne Kay <vivkay8@gmail.com>
2015-02-09 18:37:40 -0500

    adds final variables

--- js/main.js
+++ js/main.js
@@ -1,34 +1,26 @@
-// alert("hello!");
-
-var firstName = "Thomas"; // String: just some text
-console.log(firstName);
-
-// a variable is a container that holds one thing
-var isHuman = true; //Boolean: true or false
-var age = 32.1;
-
-//an array is indicated with square brackets
-// an array is a variable that can hold many things
-//in the list of variables within your array, always start counting at zero (thus below, the stegosaurus is numbered zero, the t-rex is one, and the apatosaurus is two.
-
-var dinos = ["Stegosaurus", "T-Rex", "Apatosaurus"];
-console.log(dinos[2]);
-
-// an object is indicated with curly brackets
-// the things inside an object are defined by the name we give it
-// an object is a container that holds many variables (vs. an array which holds a bunch of data)
-
-
-var trex = {
-    weight: "3 tonnes", 
-    isMeatEater: true,
-    age: 160000000
+var blueberries = {
+    latin: "Vaccinum corymbosum",
+    growsOnShrub: true,
+    energy: "240 kJ",
+    carbohydrates: "14.49",
+    protein: "0.74 g"
 };
 
-console.log(trex.age);
 
+var grapes = {
+    latin: "Vitis vinifera",
+    growsOnShrub: false,
+    energy: "288 kJ",
+    carbohydrates: "18.1 g",
+    protein: "0.72 g"
+};
 
-
-
+var redCurrant = {
+    latin: "Ribes rubrum",
+    growsOnShrub: true,
+    energy: "234 kJ",
+    carbohydrates: "13.8 g",
+    protein: "1.4 g"
+};
 
 
